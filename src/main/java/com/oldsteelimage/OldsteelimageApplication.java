0fb73a8327819b7437113767f1362f3de7e30862
oldsteelotocustom <oldsteelotocustom@gmail.com>
2023-02-27 10:06:01 +0700

    initial commit

---
+++ src/main/java/com/oldsteelimage/OldsteelimageApplication.java
@@ -0,0 +1,13 @@
+package com.oldsteelimage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OldsteelimageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OldsteelimageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oldsteelimage/OldsteelimageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oldsteelimage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OldsteelimageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
